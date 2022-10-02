d614209f514ad04e89daf55760a58f73c9fb0ab1
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2022-10-02 14:48:24 +0700

    Changed Time Calc

--- graph.js
+++ graph.js
@@ -11,7 +11,7 @@
 
 const oneDayAgo = new Date().getTime() - (1 * 24 * 60 * 60 * 1000)
 
-const oneDayAgoDateTime = oneDayAgo.getTime()
+const oneDayAgoDateTime = new Date(oneDayAgo).getTime()
 
 let chulaFolder
 
