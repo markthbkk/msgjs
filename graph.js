cfde5eddf4d155958600d48f9d2eb3b11d585087
[Mark Thomas] <[mark.i.thomas@outlook.com]>
2022-10-02 16:48:28 +0700

    Changed Time Calc

--- graph.js
+++ graph.js
@@ -1,5 +1,22 @@
 let last24Hours = new Date(new Date().setHours(new Date().getHours() - 24));
 
+
+let year = last24Hours.getUTCFullYear();
+let month = last24Hours.getUTCMonth() + 1;
+let date = last24Hours.getUTCDate();
+let hours = last24Hours.getUTCHours();
+let minutes = last24Hours.getUTCMinutes();
+let seconds = last24Hours.getUTCSeconds();
+
+date < 10 ? (date = "0" + date) : (date = date);
+month < 10 ? (month = "0" + month) : (month = month);
+hours < 10 ? (hours = "0" + hours) : (hours = hours);
+minutes < 10 ? (minutes = "0" + minutes) : (minutes = minutes);
+seconds < 10 ? (seconds = "0" + seconds) : (seconds = seconds);
+
+last24HoursTimeStamp = `${year}-${month}-${date}T${hours}:${minutes}:${seconds}Z`
+
+
 // const yyyy = last24Hours.getFullYear();
 // let mm = last24Hours.getMonth() + 1;
 // let dd = last24Hours.getDate();
@@ -93,10 +110,11 @@ console.log(healthCheckSubFolders.value);
 
 async function getMessages(mailFldr) {
 
+  console.log(last24HoursTimeStamp);
     
             return await graphClient
               .api(`/me/mailFolders/${mailFldr.id}/messages?$top=5`)
-              .filter(`receivedDateTime ge ${last24Hours}`)
+              .filter(`receivedDateTime ge ${last24HoursTimeStamp}`)
               .select("sender,subject,weblink,receivedDateTime")
               .get();
     
